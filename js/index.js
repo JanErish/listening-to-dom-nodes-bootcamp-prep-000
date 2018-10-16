dd7c815e0fa32a9571f054f9f6abcec7ebed56d8
janerishbaluca <janerishbaluca@gmail.com>
2018-10-16 22:49:36 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -11,3 +11,11 @@ const input = document.querySelector('input');
 input.addEventListener('keydown',function(e) {
   console.log(e.which);
 });
+
+input.addEventListener('keydown', function(e) {
+  if (e.which === 71) {
+    return e.preventDefault();
+  } else {
+    console.log(e.which);
+  }
+});
